980fbf6145a5df79771a96f18f6bb575c3b18030
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2022-12-17 18:46:24 +0100

    ok

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ mongoose
 
 const PORT = process.env.PORT || 8000;
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static('client/build'));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,  'client/build/index.html'));
   });
 }
 app.listen(PORT, () => {
